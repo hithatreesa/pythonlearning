abb1708b255b5c06b36e5e6138b12c1ac7b5f484
hithatreesa <hithatreesathomas369@gmail.com>
2025-11-07 12:30:25 +0530

    testing push

---
+++ sample.py
@@ -0,0 +1,10 @@
+a = int(input('enter a number' ))
+b = int(input('enter a number'))
+sum=a+b
+diff=a-b
+product=a*b
+division=a//b
+print(sum)
+print(diff)
+print(product)
+print(division)
---
+++ swap.py
@@ -0,0 +1,8 @@
+a=3
+b=6
+c=0
+
+c=a #c=3
+a=b #a=6
+b=c #b=3
+print(a,b)
